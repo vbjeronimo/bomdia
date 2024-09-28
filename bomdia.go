2a43caa73d8fa8f18194148e32f8bfd6a502b2c7
Vitor Jeronimo <vitor.jeronimo@proton.me>
2024-09-28 14:17:54 -0300

    Simplified the code a bit to get the prototype done quicker

--- bomdia.go
+++ bomdia.go
@@ -7,41 +7,35 @@ import (
 )
 
 func main() {
-	interfaces, err := net.Interfaces()
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "[ERROR] Could not get list of network interfaces: %s\n", err)
+	args := os.Args
+	if len(args) < 2 {
+		fmt.Fprintf(os.Stderr, "Usage: %s INTERFACE_NAME\n", args[0])
 		os.Exit(1)
 	}
 
+	iface, err := net.InterfaceByName(args[1])
+	if err != nil {
+		panic(err)
+	}
+
 	groupAddr := net.UDPAddr{
-		IP: net.ParseIP("224.0.0.251"),
+		IP: net.ParseIP("244.0.0.251"),
 		Port: 5353,
 	}
 
-	for _, iface := range interfaces {
-		if iface.Flags & net.FlagUp != 0 && iface.Flags & net.FlagRunning != 0 && iface.Flags & net.FlagLoopback == 0 {
-			fmt.Printf("[INFO] Setting up interface '%s'\n", iface.Name)
+	conn, err := net.ListenMulticastUDP("udp", iface, &groupAddr)
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close()
 
-			conn, err := net.ListenMulticastUDP("udp", &iface, &groupAddr)
-			if err != nil {
-				fmt.Fprintf(os.Stderr, "[ERROR] Could not open an UDP connection for interface '%s': %s\n", iface.Name, err)
-				os.Exit(1)
-			}
-			defer conn.Close()
-
-			fmt.Printf("[SUCCESS] Network interface '%s' is listening on the mDNS multicast group\n", iface.Name)
-
-			// TODO: extract this block into a function and run it in a goroutine
-			buf := make([]byte, iface.MTU)
-			for {
-				n , src, err := conn.ReadFromUDP(buf)
-				if err != nil {
-					fmt.Fprintf(os.Stderr, "[ERROR] Failed to read packet: %s\n", err)
-					os.Exit(1)
-				}
-
-				fmt.Printf("[%s:%d] %s (%d bytes)\n", src.IP, src.Port, buf[:n], n)
-			}
+	buf := make([]byte, iface.MTU)
+	for {
+		n, src, err := conn.ReadFromUDP(buf)
+		if err != nil {
+			panic(err)
 		}
+
+		fmt.Printf("[%s:%d] %s (%d bytes)", src.IP, src.Port, buf[:n], n)
 	}
 }
