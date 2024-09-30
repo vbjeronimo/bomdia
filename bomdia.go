8757aec3ae9a94dc3531b2700503840b44f9714b
Vitor Jeronimo <vitor.jeronimo@proton.me>
2024-09-30 16:57:35 -0300

    Added logic to extract info from the packet header

--- bomdia.go
+++ bomdia.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/binary"
 	"fmt"
 	"net"
 	"os"
@@ -38,6 +39,48 @@ func main() {
 			panic(err)
 		}
 
-		fmt.Printf("[%s:%d] %s (%d bytes)", src.IP, src.Port, buf[:n], n)
+		if n < 12 {
+			panic("Too few bytes received to be a proper mDNS packet")
+		}
+
+		ID := binary.BigEndian.Uint16(buf[0:2])
+
+		QR := buf[2]&0x80 != 0
+		OpCode := uint8(buf[2]>>3 & 0x0F)
+		AA := buf[2]&0x04 != 0
+		TC := buf[2]&0x02 != 0
+		RD := buf[2]&0x01 != 0
+
+		RA := buf[3]&0x80 != 0
+		Z := uint8(buf[3]>>4 & 0x07)
+		RCode := uint8(buf[3]&0x0F)
+
+		NumQuestions := binary.BigEndian.Uint16(buf[4:6])
+		NumAnswers := binary.BigEndian.Uint16(buf[6:8])
+		NumAuthorityRR := binary.BigEndian.Uint16(buf[8:10])
+		NumAdditionalRR := binary.BigEndian.Uint16(buf[10:12])
+
+		fmt.Printf("Message from '%s:%d'\n", src.IP, src.Port)
+		fmt.Printf("FULL HEADER: %b\n", buf[:12])
+
+		fmt.Printf("Query ID:        %d\n", ID)
+
+		fmt.Printf("QR Bit:          %v\n", QR)
+		fmt.Printf("OpCode:          %d\n", OpCode)
+		fmt.Printf("AA Bit:          %v\n", AA)
+		fmt.Printf("TC Bit:          %v\n", TC)
+		fmt.Printf("RD Bit:          %v\n", RD)
+
+		fmt.Printf("RA Bit:          %v\n", RA)
+		fmt.Printf("Z Bits:          %b\n", Z)
+		fmt.Printf("RCode:           %d\n", RCode)
+
+		fmt.Printf("# Questions:     %d\n", NumQuestions)
+		fmt.Printf("# Answers:       %d\n", NumAnswers)
+		fmt.Printf("# Authority RR:  %d\n", NumAuthorityRR)
+		fmt.Printf("# Additional RR: %d\n", NumAdditionalRR)
+
+		fmt.Printf("\n----------------\n")
+		
 	}
 }
